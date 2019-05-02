ae75a58eeeb70ddb64687d59f3d7d07158bd8828
AbduQuadir <50167923+AbduQuadir@users.noreply.github.com>
2019-05-02 11:54:57 +0530

    update test12

--- TestProject/src/com/test/Test12.java
+++ TestProject/src/com/test/Test12.java
@@ -6,17 +6,17 @@ public class Test12 {
 
 	public static void main(String[] args) {
 		
-     ArrayList<String> me = new ArrayList<>();
+     ArrayList<String> me = new ArrayList<String>();
      me.add("abdul");
      me.add("quadir");
      String[] un = new String[me.size()];
      un = (String[]) me.toArray(un);
      for(int i=0;i<un.length;i++){
-         /*sys = sys.substring(1, 3);
+         sys = sys.substring(1, 3);
          ArrayList<String> con = new ArrayList<>();
          con.add(sys);
          String[] due = new String[con.size()];
-         due = (String[]) con.toArray(due);*/
+         due = (String[]) con.toArray(due);
           String qudir = un[i];
           String sys = qudir.substring(1,3);
           System.out.println(sys);
